e7b8236a2c32eb8fb2cdfc9ff144ed4b71d30a30
merttekdemir <mert.tekdemir@studbocconi.it>
2023-03-25 01:26:21 +0100

    Working on more Online Learning Algorithms

--- src/OnlineLearningAlgorithms copy.jl
+++ src/OnlineLearningAlgorithms copy.jl
@@ -45,6 +45,28 @@ using Random, Distributions
         end
         return 
     end
+
+    function EpsilonGreedy(ξ, stochastic_average_reward_per_arm ;ϵ=0.1)
+        if Uniform{Float64}{0, 1} < ϵ
+            probs(ξ) = probs(ξ).*0
+            arm = Uniform{Int64}{1, length(last(stochastic_average_reward_per_arm))}
+            probs(ξ)[arm] = 1
+            return probs(ξ)
+        end
+        probs(ξ) = probs(ξ).*0
+        arm = argmax(stochastic_average_reward_per_arm)
+        probs(ξ)[arm] = 1
+        return probs(ξ)
+    end
+
+    function Hedge(ξ, reward_vector; η)
+        #full info algo
+        # η > 0
+        # https://people.eecs.berkeley.edu/~jiantao/2902021spring/scribe/EE290_Lecture_09.pdf
+        return probs(ξ) .* exp(reward_vector .* -η)
+    end
+
+
     
 
 end #Module
